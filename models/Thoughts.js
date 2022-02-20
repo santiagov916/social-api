51643747da9daf85a2ee445df9b4f8a3c392ca54
Santiago Venegas <santiagovenegas@Santiagos-MacBook-Pro.local>
2022-02-20 01:33:06 -0800

    thought model added to models directory

---
+++ models/Thoughts.js
@@ -0,0 +1,58 @@
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionText: {
+            type: String,
+            required: true,
+            min: 1,
+            max: 300,
+        },
+        username: {
+            type: String,
+            ref: 'User'
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+    },
+    {
+        toJson: {
+            getters: true
+        }
+    }
+);
+
+const ThoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        min: 1,
+        max: 35,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [ReactionSchema]
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+});
+
+const Thought = model('Thought, ThoughtSchema');
+module.exports = Thought;
